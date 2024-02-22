2b5772850027ef4108e1d510e78c1f7e8a58ccf7
Kamol774 <chust1222@gmail.com>
2024-02-22 13:26:14 +0500

    feat: integration of environmental variables

---
+++ src/server.ts
@@ -0,0 +1,5 @@
+// Architectural pattern: MVC, Dependency Injection, MVP - backend yoki frontend ni qurib beradigan suyak (arxitektura)
+// Design pattern: Middleware, Decorator  -  ma'lum bir bo'limlarni(uchastkalarni) masalasini hal qilib beradigan vositachi pattern.   
+
+import dotenv from 'dotenv';
+dotenv.config();

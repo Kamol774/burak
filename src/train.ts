66b62c0274d45e8910145857471f10f33eb79b85
Kamol774 <chust1222@gmail.com>
2024-02-22 18:44:48 +0500

    feat: G-Task done!

--- src/train.ts
+++ src/train.ts
@@ -1,1 +1,19 @@
-console.log("Hello world")
+/* G-TASK: 
+
+Shunday function tuzingki unga integerlardan iborat array pass bolsin va function bizga osha arrayning eng katta qiymatiga tegishli birinchi indexni qaytarsin.
+MASALAN: getHighestIndex([5, 21, 12, 21, 8]) return qiladi 1 sonini */
+
+
+
+function getHighestIndex(arr: Array<number>) {
+  let newArr = [...arr]
+  function sortArr(a:number, b:number) {
+    return a > b ? -1 : b > a ? 1 : 0;
+    }  
+  let m = newArr.sort(sortArr)[0]
+  console.log(`Berilgan arraydagi eng katta son ${m} va uning indeksi < ${arr.indexOf(m)} >`)
+} 
+
+getHighestIndex([1, 136, 8, 3, 156, 19, 2, 34, 55, 7])
+
+

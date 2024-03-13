a09cb099921948e9a2f220dbc75512f3d77f2827
Kamol774 <chust1222@gmail.com>
2024-03-13 12:02:36 +0500

    feat: configure product Schema model

--- src/models/Product.service.ts
+++ src/models/Product.service.ts
@@ -1,8 +1,16 @@
+import ProductModel from "../schema/Product.model";
 
 
 
 class ProductService {
+  private readonly productModel;
+
+  constructor() {
+    this.productModel = ProductModel; // productModel ni schema dagi ProductModel iga tenglab olamz 
+  }
+
 
 }
+
 
 export default ProductService;  

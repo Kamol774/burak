81540626b7f4b800f853c56c4eaf3c081a71ed69
Kamol774 <chust1222@gmail.com>
2024-03-18 11:03:52 +0500

    N-Task done!

--- src/train.ts
+++ src/train.ts
@@ -1,19 +1,33 @@
+/* N-TASK: 
+
+Shunday function yozing, u string qabul qilsin va string palindrom yani togri oqilganda ham, orqasidan oqilganda ham bir hil oqiladigan soz ekanligini aniqlab boolean qiymat qaytarsin.
+MASALAN: palindromCheck("dad") return true;  palindromCheck("son") return false;
+*/
+
+function palindromCheck(word: string) {
+  const word2: string = word.split('').reverse().join('').toString()
+  return word === word2
+}
+
+console.log(palindromCheck("dad"))
+console.log(palindromCheck("son"))
+
+
+//////////////////////////////////////////////////////////////////////////
+
 /* M-TASK:
 
 Shunday function yozing, u raqamlardan tashkil topgan array qabul qilsin va array ichidagi har bir raqam uchun raqamni ozi va hamda osha raqamni kvadratidan tashkil topgan object hosil qilib, hosil bolgan objectlarni array ichida qaytarsin.
 MASALAN: getSquareNumbers([1, 2, 3]) return [{number: 1, square: 1}, {number: 2, square: 4}, {number: 3, square: 9}];
 */
 
-function getSquareNumbers(numbers: number[]) {
-  return numbers.map(ele => { return { number: ele, square: ele * ele } })
-}
+// function getSquareNumbers(numbers: number[]) {
+//   return numbers.map(ele => { return { number: ele, square: ele * ele } })
+// }
 
-const result = getSquareNumbers([1, 2, 3]);
-console.log(result);
-console.log("typeof result:", typeof result)
-
-
-
+// const result = getSquareNumbers([1, 2, 3]);
+// console.log(result);
+// console.log("typeof result:", typeof result)
 
 
 //////////////////////////////////////////////////////////////////////////

d2f65e6a815300dfa87c2e8ce0be659b2572ce19
Kamol774 <chust1222@gmail.com>
2024-03-20 09:35:45 +0500

    fix: modify signup

--- src/public/js/signup.js
+++ src/public/js/signup.js
@@ -1,8 +1,35 @@
 console.log("Signup frontend javascript file");
 
+$(function () {  // rasm yuklashdan oldin yuklovchi qanday rasm yuklaganini kichik hajmda bo'lsa ham ko'rishi uchun mantiq
+  const fileTarget = $(".file-box .upload-hidden"); // rasm yuklashda qatnashadigan input clasini belgilayapmiz
+  let filename;
+
+  fileTarget.on("change", function () {
+    if (window.FileReader) {
+      const uploadFile = $(this)[0].files[0]; // upload qilingan file ni qo'lga olyapmiz
+      const fileType = uploadFile["type"];
+      const validImageType = ["image/jpg", "image/jpeg", "image/png"]; // faqatkina shu type dagi fayllarni qabul qilsin
+      if (!validImageType.includes(fileType)) {
+        alert("Please insert only jpg, jpeg, png!");
+      } else {
+        if (uploadFile) {
+          console.log(URL.createObjectURL(uploadFile)); // uploadFile ni url ga taqdim etyapmiz va createObjectURL ni uploadFile orqali hosil qilyapmiz 
+          $(".upload-img-frame")  // default turgan rasmni upload qileyatkan rasmga o'zgartirish mantig'i
+            .attr("src", URL.createObjectURL(uploadFile))
+            .addClass("success");
+        }
+        filename = $(this)[0].files[0].name; //upload qilinayotkan rasm nomi(name)ni filename ga tenglayapmiz
+      }
+
+      $(this).siblings(".upload-name").val(filename); //upload qilinayotkan rasm nomi bizga ko'rinib turishi uchun mantiq
+    }
+  })
+});
+
+
 
 $(function () { });
-
+// signup jarayoni uchun frontend validation quryapmiz
 function validateSignupForm() { // ejs -> form dagi onsubmit bilan bog'lanadi
   const memberNick = $(".member-nick").val()
   const memberPhone = $(".member-phone").val()
@@ -13,9 +40,16 @@ function validateSignupForm() { // ejs -> form dagi onsubmit bilan bog'lanadi
     alert("Please insert all required inputs");
     return false
   }
+
   if (memberPassword !== confirmPassword) {
     alert("Password differs, please check!");
     return false
   }
-  return false;
+
+  // quyida member-image ni qabul qilib olish mantig'i:
+  const memberImage = $(".member-image").get(0).files[0] ? $(".member-image").get(0).files[0].name : null;
+  if (!memberImage) {
+    alert("Please insert restaurant image!");
+    return false;
+  }
 }

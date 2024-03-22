2ba12955b247f4efb98c94b1e55fb0be77de0460
Kamol774 <chust1222@gmail.com>
2024-03-22 16:43:53 +0500

    P-Task done

--- src/train.ts
+++ src/train.ts
@@ -1,19 +1,45 @@
+/*
+P - TASK:
+
+Shunday function yozing, u object qabul
+ qilsin va arrayni object arrayga otkazib arrayni qaytarsin qaytarsin.
+MASALAN: objectToArray( {a: 10, b: 20}) return [['a', 10], ['b', 20]]
+*/
+
+function objectToArray(val: any) {
+  const result = [];
+  for (let key in val) {
+    if (val.hasOwnProperty(key)) {
+      result.push([key, val[key]]);
+    }
+  }
+  return result;
+}
+
+const obj = { a: 10, b: 20 };
+const result = objectToArray(obj);
+console.log("result:", result);
+
+
+
+//////////////////////////////////////////////////////////////////////////
+
 /* O-TASK:
 
 Shunday function yozing, u har xil valuelardan iborat array qabul qilsin va array ichidagi sonlar yigindisini hisoblab chiqqan javobni qaytarsin.
 MASALAN: calculateSumOfNumbers([10, "10", {son: 10}, true, 35]) return 45
 */
 
-function calculateSumOfNumbers(arr: any[]) {
-  let result = 0
-  for (let ele of arr)
-    if (typeof ele == 'number') {
-      result += ele
-    }
-  return result
-}
+// function calculateSumOfNumbers(arr: any[]) {
+//   let result = 0
+//   for (let ele of arr)
+//     if (typeof ele == 'number') {
+//       result += ele
+//     }
+//   return result
+// }
 
-console.log(calculateSumOfNumbers([10, "10", 3, { son: 10 }, null, "8", true, 35]))
+// console.log(calculateSumOfNumbers([10, "10", 3, { son: 10 }, null, "8", true, 35]))
 
 
 //////////////////////////////////////////////////////////////////////////

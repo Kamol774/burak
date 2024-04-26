a641858df37a6749bbfcdbcecd5d89b4de5636f3
Kamol774 <chust1222@gmail.com>
2024-04-26 10:23:19 +0500

    ZE task done

--- src/train.ts
+++ src/train.ts
@@ -1,15 +1,39 @@
+/* ZE-TASK:
+
+Shunday function yozing, uni  string parametri bolsin. String ichida takrorlangan harflarni olib tashlab qolganini qaytarsin
+MASALAN: removeDuplicate('stringg') return 'string' */
+
+
+function removeDuplicate(str: string) {
+  let count = 0;
+  let strArray = str.split("");
+  for (let i = 0; i < strArray.length; i++) {
+    for (let j = i + 1; j < strArray.length; j++) {
+      if (strArray[i] == strArray[j]) {
+        strArray.splice(j, 1)
+        count++;
+        break;
+      }
+    }
+  }
+  return strArray
+}
+
+console.log(removeDuplicate("heeloo"))
+
+
 /* ZD-TASK:
 
 Shunday function yozing, uni number, array va number parametrlari bolsin va berilgan 1-parametr numberga teng indexni array ichidan topib 3-parametrdagi raqam bilan almashtirib yangilangan arrayni qaytarsin
 MASALAN: changeNumberInArray(1, [1,3,7,2], 2) return [1,2,7,2]   */
 
 
-function changeNumberInArray(num1: number, arr: number[], num2: number) {
-  arr[num1] = num2;
-  return arr;
-}
+// function changeNumberInArray(num1: number, arr: number[], num2: number) {
+//   arr[num1] = num2;
+//   return arr;
+// }
 
-console.log(changeNumberInArray(1, [1, 3, 7, 2], 2))
+// console.log(changeNumberInArray(1, [1, 3, 7, 2], 2))
 
 
 

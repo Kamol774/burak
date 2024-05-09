227d8f96918a9a3272660772b990249fea3625bd
Kamol774 <chust1222@gmail.com>
2024-05-09 23:07:56 +0500

    ZK task done

--- src/train.ts
+++ src/train.ts
@@ -1,19 +1,37 @@
+/*  ZK-TASK:
+
+Shunday function yozing, u har soniyada bir marta consolega 1 dan 5 gacha bolgan raqamlarni chop etsin va 5 soniyadan keyin ishini toxtatsin.
+MASALAN: printNumbers()  */
+
+function printNumbers(): void {
+  let i = 1;
+  const intervalId = setInterval(() => {
+    console.log(i);
+    i++;
+    if (i > 5) {
+      clearInterval(intervalId);
+    }
+  }, 1000);
+}
+
+printNumbers();
+
 /* ZJ-TASK:
 
 Shunday function yozing, u berilgan arrayni ichidagi numberlarni qiymatini hisoblab qaytarsin.
 MASALAN: reduceNestedArray([1, [1, 2, [4]]]) return 8  */
 
-function reduceNestedArray(arr: any[]): number {
-  return arr.reduce((sum, element) => {
-    if (Array.isArray(element)) {
-      return sum + reduceNestedArray(element);
-    } else {
-      return sum + element;
-    }
-  }, 0);
-}
-const result = reduceNestedArray([1, [1, 2, [4]]]);
-console.log(result);
+// function reduceNestedArray(arr: any[]): number {
+//   return arr.reduce((sum, element) => {
+//     if (Array.isArray(element)) {
+//       return sum + reduceNestedArray(element);
+//     } else {
+//       return sum + element;
+//     }
+//   }, 0);
+// }
+// const result = reduceNestedArray([1, [1, 2, [4]]]);
+// console.log(result);
 
 /* ZH-TASK:
 
